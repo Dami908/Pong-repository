f609be63e1a0b89008f2d8b7ceba65ef0bf6af60
Emmanuel <jamestame787@gmail.com>
2019-10-02 13:20:54 -0400

    completed solution
    complete solution

---
+++ Assets/ballscript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballscript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.velocity = new Vector2 (8f, 8f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x >= 11.14f)
+        {
+            this.transform.position = new Vector3(0f, 0f, 0f);
+        }
+
+        if (this.transform.position.x >= -11.14f)
+        {
+            this.transform.position = new Vector3(0f, 0f, 0f);
+        }
+        
+
+
+    }
+}
---
+++ Assets/countscore.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class countscore : MonoBehaviour
+{
+    public Text scoreboard;
+    public GameObject ball;
+
+    private int paddle_1_score = 0;
+    private int paddle_2_score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GameObject.Find("Ball");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ball.transform.position.x >= 13f)
+        {
+            paddle_1_score++;
+        }
+
+        if (ball.transform.position.x <= -13f)
+        {
+            paddle_2_score++;
+        }
+
+        scoreboard.text = (paddle_1_score.ToString()) + "-" + (paddle_2_score.ToString());
+
+        print(paddle_1_score + "-" + paddle_2_score);
+        
+
+    }
+}
